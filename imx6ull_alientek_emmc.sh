a86c0aeba3a5dc81f545ad18090d74f92ec8526b
perrypengwang <perrypengwang@gmail.com>
2024-11-17 21:13:49 +0800

    添加Readme

--- imx6ull_alientek_emmc.sh
+++ imx6ull_alientek_emmc.sh
@@ -2,4 +2,4 @@
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_alientek_emmc_defconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j16
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j6
